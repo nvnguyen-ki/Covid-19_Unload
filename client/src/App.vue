0808c14f280fa8e18ab3cdbd1c62dd4fd91ddb5d
Nguyen Nguyen <nguyentwotimes@gmail.com>
2020-06-18 15:42:00 -0400

    register setup #4

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet">
     <router-view/>
   </div>
 </template>
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="">
-   <h1> register </h1>
-   <input type="text" v-model="email" name="email" placeholder="email"/>
-   <br>
-   <input type="password" name="password" v-model="password" placeholder="password"/>
-  <br>
-  <div class="error" v-html="error" />
-  <button @click=register>Register</button>
+  <div class="page">
+    <div class="register">
+      <h3> Register </h3>
+      <input type="text" v-model="email" name="email" placeholder="email"/>
+      <br>
+      <input type="password" name="password" v-model="password" placeholder="password"/>
+      <br>
+      <div class="error" v-html="error" />
+      <button @click=register>register</button>
+    </div>
   </div>
 </template>
 
@@ -36,7 +38,15 @@ export default {
 </script>
 
 <style scoped>
+.register {
+  width: 50%;
+  margin:auto;
+  border:1px solid rgb(146, 146, 146);
+  text-align: center;
+  font-family: 'Inconsolata',monospace;
+}
+
 .error {
-  color:royalblue;
+  color:rgb(235, 132, 107);
 }
 </style>
