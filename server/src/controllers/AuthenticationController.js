377bd88124a24decbf91509b6c4027e30646e081
Nguyen Nguyen <nguyentwotimes@gmail.com>
2020-06-19 19:14:11 -0400

    jwt token added + bcrypt

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -1,51 +1,60 @@
 const {User} = require('../models')
+const jwt = require('jsonwebtoken')
+const config = require('../config/config')
+//JSON Web Tokens are an open, industry standard RFC 7519 method for representing claims securely between two parties.
+function jwtSignUser (user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  })
+}
+
 module.exports = {
-    async register (req, res) {
-        try {
-            const user = await User.create(req.body)
-            const userJson = user.toJSON()
-            res.send({
-                user: userJson
-            })
-        } catch (err) {
-            res.status(400).send({
-                error:'Email has already been used.'
-            })
-        }
-    },
-
-    async login (req, res) {
-        try {
-            const {email,password} = req.body
-            const user = await User.findOne({
-                where: {
-                    email: email
-                }
-            })
-            if (!user) {
-                res.status(403).send({
-                    error:'the login information was incorrect.'
-                })
-            }
-            console.log(password,user.password)
-            console.log(email,user.email)
-            const isPasswordValid = password === user.password
-            if (!isPasswordValid) {
-                return res.status(403).send({
-                    error:'Incorrect password.'
-                })
-            }
-            const userJson = user.toJSON()
-            res.send({
-              user: userJson
-            })
-        } catch (err) {
-            res.status(500).send({
-                error:'Invalid login information.'
-            })
-        }
+  async register (req, res) {
+    try {
+      const user = await User.create(req.body)
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (err) {
+      res.status(400).send({
+        error: 'This email account is already in use.'
+      })
     }
+  },
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
 
+      if (!user) {
+        return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
 
+      const isPasswordValid = user.comparePassword(password)
+      if (!isPasswordValid) {
+        return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
 
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to log in'
+      })
+    }
+  }
 }
