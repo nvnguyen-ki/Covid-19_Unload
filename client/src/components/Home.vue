0522ad15baaa172607c30e1b70eb82c13f8b7a7a
Nguyen Nguyen <nguyentwotimes@gmail.com>
2020-06-23 19:53:14 -0400

    scrape code change.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,13 @@
+
 <template>
   <div class="page" style="margin-top:20px;" >
     <div class="scrape_box" >
       <div id="Infobox">
-      <input type="text" name="scrape" placeholder="url"/>
+      <input type="text" ref="url" name="url" placeholder="url"/>
       <br>
-       <input type="text" name="scrape" placeholder="Email, phone, etc..."/>
+       <!-- <input type="text" name="scrape" placeholder="Email, phone, etc..."/> -->
       </div>
-      <div class="error" v-html="error" />
-      <v-btn text small style="margin: 15px;font-size:12px; border:1px solid rgb(146, 146, 146)">Scrape</v-btn>
+      <v-btn text small style="margin: 15px;font-size:12px; border:1px solid rgb(146, 146, 146)" onClick="scrapeRealtor()">Scrape</v-btn>
       <p id="congrats"></p>
     </div>
   </div>
