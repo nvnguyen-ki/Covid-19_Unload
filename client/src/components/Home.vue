9625049bd6329c8752b4d346b70262c4adfe0805
Nguyen Nguyen <nguyentwotimes@gmail.com>
2020-06-23 17:05:48 -0400

    scrapebox set-up in home.

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,20 +1,22 @@
 <template>
-  <div class="Home">
-    test
+  <div class="page" style="margin-top:20px;" >
+    <div class="scrape_box" >
+      <div id="Infobox">
+      <input type="text" name="scrape" placeholder="url"/>
+      <br>
+       <input type="text" name="scrape" placeholder="Email, phone, etc..."/>
+      </div>
+      <div class="error" v-html="error" />
+      <v-btn text small style="margin: 15px;font-size:12px; border:1px solid rgb(146, 146, 146)">Scrape</v-btn>
+      <p id="congrats"></p>
+    </div>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'Home'
-}
-</script>
-
 <style scoped>
-.Home {
-  border:1px solid black;
-  width:auto;
+.scrape_box {
+  width:50;
+  text-align: center;
   margin:auto;
 }
 </style>
